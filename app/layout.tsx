be0eb33ea9163b21602326d8e6ff0b36c9cf1e7e
samirtharaj7-creator <samirtharaj.7@gmail.com>
2026-07-26 10:12:27 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { GlobalFooter, GlobalShell } from "@/components/global-shell";
 import { ReadingProgressBar } from "@/components/reading-progress";
 import { RouteStyling } from "@/components/route-styling";
-import { SiteHeader } from "@/components/site-header";
 import "./globals.css";
 import "./global-shell.css";
 import "./philippians-theme.css";
@@ -33,7 +32,6 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <RouteStyling />
         <GlobalShell />
         <ReadingProgressBar />
-        <SiteHeader />
         {children}
         <GlobalFooter />
       </body>
